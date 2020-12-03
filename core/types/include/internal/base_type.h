7fff726ee7e5b8046909b3101be8736d91e0e422
NitroZeus <realantiwanted@gmail.com>
2020-12-03 10:49:05 +0330

    Get type base

--- core/types/include/internal/base_type.h
+++ core/types/include/internal/base_type.h
@@ -1,4 +1,16 @@
 #ifndef SIMPLE_JSON_BASE_TYPE_H
 #define SIMPLE_JSON_BASE_TYPE_H
-
+#include <string>
+class BaseItem {
+private:
+    union {
+        unsigned int content_int;
+        float content_float;
+        std::string content_string;
+    };
+public:
+    template<typename T>
+    T get_data() {
+    }
+};
 #endif
--- main.cpp
+++ main.cpp
@@ -3,5 +3,7 @@
 
 int main() {
     std::cout << "Start Simple JSON Project!" << std::endl;
+    BaseJson *a {new JsonItem<int> {10}};
+    std::cout << "Data is " << a -> get_data<int>() << std::endl;
     return 0;
 }
