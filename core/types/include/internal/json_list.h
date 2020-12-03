6f1e3732c73d414bb0832bf44ce9ee38ec7a3658
NitroZeus <realantiwanted@gmail.com>
2020-12-03 14:51:27 +0330

    Removing extra files

--- core/types/include/internal/json_list.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIMPLE_JSON_JSON_LIST_H
-#define SIMPLE_JSON_JSON_LIST_H
-#include "base_type.h"
-#include <vector>
-class JsonList : public JsonItem, public std::vector<JsonItem> {
-
-};
-#endif //SIMPLE_JSON_JSON_LIST_H
