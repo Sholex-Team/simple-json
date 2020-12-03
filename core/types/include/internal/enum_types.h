1461ea94ce0424193c65a484658383e2221c6e47
rez <rezfd99@gmail.com>
2020-12-03 10:57:27 +0330

    add enumeration types

---
+++ core/types/include/internal/enum_types.h
@@ -0,0 +1,10 @@
+#ifndef SIMPLE_JSON_ENUM_TYPES_H
+#define SIMPLE_JSON_ENUM_TYPES_H
+
+enum all_types {
+    integer_type,
+    float_type,
+    string_type,
+};
+
+#endif //SIMPLE_JSON_ENUM_TYPES_H
