626453146bcb2aed0170319f7a68fc3bbb9e7c42
rez <rezfd99@gmail.com>
2020-12-03 13:04:42 +0330

    set used_type in initial class

--- core/types/src/base_type.cpp
+++ core/types/src/base_type.cpp
@@ -2,15 +2,15 @@
 #include "enum_types.h"
 #include "base_exception.h"
 
-JsonItem::JsonItem(double data) : data_double {data} {}
+JsonItem::JsonItem(double data) : data_double {data}, used_type {DataType::double_type} {}
 
-JsonItem::JsonItem(int data) : data_int {data} {}
+JsonItem::JsonItem(int data) : data_int {data}, used_type(DataType::integer_type) {}
 
-JsonItem::JsonItem(float data) : data_float {data} {}
+JsonItem::JsonItem(float data) : data_float {data}, used_type(DataType::float_type) {}
 
-JsonItem::JsonItem(std::string * data) : data_string {std::move(data)} {}
+JsonItem::JsonItem(std::string * data) : data_string {data}, used_type(DataType::string_type) {}
 
-JsonItem::JsonItem(std::string &&data) : data_string {std::move(&data)} {}
+JsonItem::JsonItem(std::string &&data) : data_string {&data}, used_type(DataType::string_type) {}
 
 JsonItem::operator int () {
     if (used_type == DataType {0}) {
