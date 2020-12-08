655b82b41dbd0b4e59cf17d029804acdaaa4e673
NitroZeus <realantiwanted@gmail.com>
2020-12-08 07:10:45 +0330

    Removing Dump class

--- src/serializer.cpp
+++ src/serializer.cpp
@@ -16,28 +16,24 @@ std::string serializer::dumps(const Json & json, size_t local_indent) {
     return std::move(stream.str());
 }
 
-Dump::Dump(const Json & json, const std::string & file_name, size_t local_indent) :
-file_stream {file_name} {
-    if (!file_stream) {
+void serializer::dump(Json & json, const std::string & file_name, size_t local_indent) {
+    std::ofstream file_stream;
+    try {
+        if (!file_stream) {
+            if (file_stream.is_open()) {
+                file_stream.close();
+            }
+            throw WritingToFileException();
+        }
+        size_t old_indent = switch_indent(local_indent);
+        file_stream << json;
+        indent_length = old_indent;
+        file_stream.close();
+    } catch (std::exception & e) {
         if (file_stream.is_open()) {
             file_stream.close();
         }
-        throw WritingToFileException();
     }
-    size_t old_indent = switch_indent(local_indent);
-    file_stream << json;
-    indent_length = old_indent;
-    file_stream.close();
-}
-
-Dump::~Dump() {
-    if (file_stream.is_open()) {
-        file_stream.close();
-    }
-}
-
-void serializer::dump(Json & json, const std::string & file_name, size_t local_indent) {
-    Dump(json, file_name, local_indent);
 }
 
 #pragma endregion
