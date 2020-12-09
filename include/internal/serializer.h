2be916ed0c88cb433872157e5f4dbe2fe80594d8
NitroZeus <realantiwanted@gmail.com>
2020-12-09 16:35:24 +0330

    Revert "Removing Dump class"
    This reverts commit 655b82b4

--- include/internal/serializer.h
+++ include/internal/serializer.h
@@ -13,6 +13,16 @@ using namespace simple_json::types;
 
 namespace simple_json::serializer {
     std::string dumps(const Json &, size_t = 0);
+
+    class Dump {
+    private:
+        std::ofstream file_stream;
+    public:
+        // Constructor
+        Dump(const Json &, const std::string &, size_t);
+        ~Dump();
+    };
+
     void dump(Json &, const std::string &, size_t = 0);
 }
 
--- src/serializer.cpp
+++ src/serializer.cpp
@@ -16,23 +16,28 @@ std::string serializer::dumps(const Json & json, size_t local_indent) {
     return std::move(stream.str());
 }
 
-void serializer::dump(Json & json, const std::string & file_name, size_t local_indent) {
-    std::ofstream file_stream;
-    try {
-        file_stream.open(file_name);
-        if (!file_stream) {
-            throw WritingToFileException();
-        }
-        size_t old_indent = switch_indent(local_indent);
-        file_stream << json << std::endl;
-        indent_length = old_indent;
-        file_stream.close();
-    } catch (std::exception & e) {
+Dump::Dump(const Json & json, const std::string & file_name, size_t local_indent) :
+        file_stream {file_name} {
+    if (!file_stream) {
         if (file_stream.is_open()) {
             file_stream.close();
         }
-        throw e;
+        throw WritingToFileException();
     }
+    size_t old_indent = switch_indent(local_indent);
+    file_stream << json;
+    indent_length = old_indent;
+    file_stream.close();
+}
+
+Dump::~Dump() {
+    if (file_stream.is_open()) {
+        file_stream.close();
+    }
+}
+
+void serializer::dump(Json & json, const std::string & file_name, size_t local_indent) {
+    Dump(json, file_name, local_indent);
 }
 
 #pragma endregion
