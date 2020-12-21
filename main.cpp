087e1f72ab4e653852aea144c6695d7cc999aaf7
NitroZeus <realantiwanted@gmail.com>
2020-12-21 20:14:42 +0330

    constructor bug

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include "string"
 
 using namespace simple_json;
+using namespace types;
 using namespace deserializer;
 
 int main() {
+    Json {10};
     std::string test_json {"[1, 2, 3]"};
     loads(test_json);
 }
