f65042a3f7715d40182337e86795a34d5b3ee167
rez <rezfd99@gmail.com>
2021-01-06 20:22:59 +0330

    bug test

--- main.cpp
+++ main.cpp
@@ -9,12 +9,14 @@ using namespace types;
 using namespace indent;
 
 int main() {
-    Json p(loads(R"({
-        "121": null,
-        "22325": true,
-        "3": [true, 1111, "12345", null],
-        "3561984152": [true, 1111, "12345", null, false]
-})"));
+    Json p(loads(R"([
+  {
+      "voters": [],
+      "accept_vote": [
+        405154
+        ]
+    }
+  ])"));
     std::cout << set_indent() << p;
     return 0;
 }
