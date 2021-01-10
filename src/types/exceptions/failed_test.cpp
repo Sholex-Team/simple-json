60f58ef9773c2b7202de72dd8f2da86b4dcdb044
rez <rezfd99@gmail.com>
2021-01-10 11:45:58 +0330

    reformat failed_test.cpp

--- src/types/exceptions/failed_test.cpp
+++ src/types/exceptions/failed_test.cpp
@@ -1,7 +1,6 @@
 #include "types/exceptions/failed_test.h"
 
 namespace simple_json::types::exceptions {
-
     const char * FailedTest::what() const noexcept {
         return "JSON Patch test failed !";
     }
