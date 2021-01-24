1a5d2dc968e2885434c269539dd8de62494741ee
NitroZeus <realantiwanted@gmail.com>
2021-01-24 19:07:16 +0330

    changing test case

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ using namespace types;
 using namespace indent;
 
 int main() {
-    Json src(loads(R"([1, 2, 3, [4, 6], 7])"));
-    Json dst(loads(R"([3, 3, 3, 3, 7, 7])"));
+    Json src(loads(R"([1, 3, [1, 3, 3]])"));
+    Json dst(loads(R"([1, 3, [4, 5, 6]])"));
     JsonPatch patch {src.get_diff(dst)};
     std::cout << patch.get_json() << std::endl;
     return 0;
