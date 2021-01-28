0f7bb6593991cf9358d00bd29c2a276835ef2e13
rez <rezfd99@gmail.com>
2021-01-28 09:24:20 +0330

    bug syntax

--- main.cpp
+++ main.cpp
@@ -8,10 +8,9 @@ using namespace indent;
 
 int main() {
     Json src (loads(R"({"a": "reza", "b": [1, 2, 3, 4, 5, 6], "c": "mosi", "d": [1, 2, 3, 4, 5, 6]})"));
-    Json dst (loads(R"({"a": "ali", "b": [4, 2, 1], "c": "mosi", "z": [1, 2, 3, 4, 5, 6]})"));
-    JsonPatch patch{dst.get_diff(src)};
+    JsonPatch patch {loads(R"([{ "op": "add", "value": "test", "path": "/d~0" }])")};
     std::cout << patch.get_json() << std::endl;
-    patch.apply(dst);
-    std::cout << dst << std::endl;
+    patch.apply(src);
+    std::cout << src << std::endl;
     return 0;
 }
