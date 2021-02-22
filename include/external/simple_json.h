9561aa77f784ce3d351951451b65011f2ec0e582
rez <rezfd99@gmail.com>
2021-02-22 09:59:34 +0330

    change and fix simple json header

--- include/external/simple_json.h
+++ include/external/simple_json.h
@@ -2,21 +2,29 @@
 #define SIMPLE_JSON_SIMPLE_JSON_H
 
 #include "types/json.h"
-#include "indent.h"
 #include "types/array_type.h"
-#include "types/exceptions/bad_conversion.h"
-#include "types/exceptions/base_exception.h"
 #include "types/enum_types.h"
-#include "types/exceptions/invalid_index.h"
 #include "types/json_key.h"
 #include "types/json_object.h"
-#include "types/exceptions/stream_exceptions.h"
 #include "types/json_patch.h"
 #include "types/json_pointer.h"
+
 #include "indent.h"
 #include "serializer.h"
 #include "deserializer.h"
-#include "types/exceptions/deserializer_exceptions.h"
 #include "json_utils.h"
+
+#include "types/exceptions/invalid_merge.h"
+#include "types/exceptions/invalid_operation.h"
+#include "types/exceptions/invalid_operator.h"
+#include "types/exceptions/invalid_pointer.h"
+#include "types/exceptions/invalid_index.h"
+#include "types/exceptions/invalid_patch_operation.h"
+#include "types/exceptions/base_exception.h"
+#include "types/exceptions/bad_conversion.h"
+#include "types/exceptions/deserializer_exceptions.h"
+#include "types/exceptions/stream_exceptions.h"
+#include "types/exceptions/failed_test.h"
+#include "types/exceptions/iterator_exceptions.h"
 
 #endif //SIMPLE_JSON_SIMPLE_JSON_H
