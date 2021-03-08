da42f9053ae621862427a36dca31e81ec0dea1b2
NitroZeus <realantiwanted@gmail.com>
2021-03-08 18:08:51 +0330

    Editing example

--- examples/indent.cpp
+++ examples/indent.cpp
@@ -1,9 +1,9 @@
 #include <simple_json.h>
 
 using namespace simple_json;
-using namespace ::types;
-using namespace ::deserializer;
-using namespace ::indent;
+using namespace simple_json::types;
+using namespace simple_json::deserializer;
+using namespace simple_json::indent;
 
 int main() {
     Json json(loads(R"({
