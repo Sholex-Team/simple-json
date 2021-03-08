d5f4e57cfa5fd9672b67ca31dd90176c48c749f0
NitroZeus <realantiwanted@gmail.com>
2021-03-08 18:08:00 +0330

    Renaming examples

--- include/internal/indent.h
+++ include/internal/indent.h
@@ -5,13 +5,14 @@
 #include <ostream>
 
 namespace simple_json::indent {
+    /** @example indent.cpp
+     * This is an example of how to use the set_indent function.
+    */
+
     struct SetIndent {};
 
     extern size_t indent_length;
 
-    /** \example indent/set_indent.cpp
-         * This is an example of how to use the set_indent function.
-    */
     inline SetIndent set_indent(size_t length = 2)  {
         indent_length = length;
         return {};
