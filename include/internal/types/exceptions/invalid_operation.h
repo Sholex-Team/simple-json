6e1ced234e332eb3aae97209eb5e98150ba6423a
NitroZeus <realantiwanted@gmail.com>
2021-03-09 19:58:24 +0330

    Added documents for invalid_o

--- include/internal/types/exceptions/invalid_operation.h
+++ include/internal/types/exceptions/invalid_operation.h
@@ -5,11 +5,23 @@
 #include "../enum_types.h"
 
 namespace simple_json::types::exceptions {
+    /*!
+     * @brief Invalid Operation exception.
+     *
+     * This exception throws when there is an invalid operation happening on a Json object.
+     */
     class InvalidOperation : public base_exceptions::JsonException {
     private:
         DataType used_type;
     public:
         // Constructors
+        /*!
+         * @brief InvalidOperation constructor.
+         *
+         * This constructor takes an optional argument which represents Json object data type being used in an
+         * operation.
+         * @param used_type The type of Json object which is being used in the operation.
+         */
         explicit InvalidOperation(DataType used_type = DataType::unknown);
 
         // Public Methods
