29fdaabfb6a21b5746084bda168ea690503636db
NitroZeus <realantiwanted@gmail.com>
2021-03-09 16:38:55 +0330

    Removing extra documentation

--- include/internal/types/exceptions/deserializer_exceptions.h
+++ include/internal/types/exceptions/deserializer_exceptions.h
@@ -12,7 +12,6 @@ namespace simple_json::deserializer::exceptions {
      */
     class ParsingException : public base_exceptions::JsonException {
     public:
-        /*! Parsing error types */
         enum Errors : std::uint8_t {
             illegal_array_split, /*!< Used when there is an illegal "," inside the serialized JSON. */
             illegal_key_split, /*!< Used when there is an illegal ":" inside the serialized JSON. */
