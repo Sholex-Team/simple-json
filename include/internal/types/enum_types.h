ff65717f91dfd6c51347512abd4fe8beb6befb74
NitroZeus <realantiwanted@gmail.com>
2021-03-10 20:39:58 +0330

    Added documentation for enum types

--- include/internal/types/enum_types.h
+++ include/internal/types/enum_types.h
@@ -6,22 +6,52 @@
 
 namespace simple_json::types {
     enum class DataType : std::uint8_t {
-        integer_type,
-        string_type,
-        double_type,
-        boolean_type,
+        integer_type, /*!< This type represents a signed long int which is biggest integer data type in CPP .*/
+        string_type, /*!< This type represents a std::string type which is used instead of c-string. */
+        double_type, /*!< This type represents a double which is the fractional data type. */
+        boolean_type, /*!< This type represents booleans used in JSON */
+
+        /*!
+         * This type represents arrays in JSON that are stored in a Json object using Array type.
+         */
         array_type,
+
+        /*!
+         * This type represents JSON Objects in JSON that are stored in a Json object using JsonObject type.
+         */
         json_object_type,
-        null_type,
-        string_key_type,
+        null_type, /*!< This type represents null values in JSON. */
+        string_key_type, /*!< This type represents JsonKey which is used inside a JsonObject as key of the map. */
+
+        /*!
+         * This type represents special types such as null, false, and true. it's only used inside the deserializer
+         * and there is no use case for it outside.
+         */
         special_type,
+
+        /*!
+         * This type is used when an empty Json object is created without any type or data. Usually there is no
+         * use case for it outside of deserializer and PatchBuilder.
+         */
         unknown
     };
 
+    /*! @enum DataType
+     * @brief JSON data types.
+     *
+     * These types will be used for specifying Json object inner type.
+     */
+
     enum class IteratorTypes : std::uint8_t  {
-        array_iterator_type,
-        json_object_iterator_type
+        array_iterator_type, /*!< This type will be used if iterator is Array based. */
+        json_object_iterator_type /*!< This type will be used if iterator is JsonObject based. */
     };
+
+    /*! @enum IteratorTypes
+     * @brief Json Iterator types.
+     *
+     * This types will be used inside a JsonIterator based iterator to specify it's inner type.
+     */
 }
 
 #endif //SIMPLE_JSON_ENUM_TYPES_H
