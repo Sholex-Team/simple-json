f6083ac35e8c9c507f19e8d01e61f999c3f1ffaa
NitroZeus <realantiwanted@gmail.com>
2021-03-10 17:48:24 +0330

    Added documentation for stream exceptions

--- include/internal/types/exceptions/stream_exceptions.h
+++ include/internal/types/exceptions/stream_exceptions.h
@@ -6,16 +6,31 @@
 
 namespace simple_json {
     namespace serializer::exceptions {
+        /*!
+         * @brief Stream Insertion exception.
+         *
+         * This exception throws when serializer fail to write into a output stream.
+         */
         class StreamInsertionException : public simple_json::base_exceptions::JsonException {
         public:
             const char * what() const noexcept override;
         };
 
+        /*!
+         * @brief Writing To File exception.
+         *
+         * This exception throws when serializer fail to write into a file or the file is not open for any reason.
+         */
         class WritingToFileException : public simple_json::base_exceptions::JsonException {
         public:
             const char * what() const noexcept override;
         };
     }
+    /*! @namespace simple_json::serializer::exceptions
+     * @brief A namespace dedicated to serializer exceptions.
+     *
+     * This namespace contains all the exceptions related to serializer and serializing operations.
+     */
 
     namespace deserializer::exceptions {
         class ReadingFromFileException : public simple_json::base_exceptions::JsonException {
@@ -23,6 +38,11 @@ namespace simple_json {
             const char * what() const noexcept override;
         };
     }
+    /*! @namespace deserializer::exceptions
+     * @brief A namespace dedicated to deserializer exceptions.
+     *
+     * This namespace contains all the exceptions related to deserializer and deserializing operations.
+     */
 }
 
 #endif //SIMPLE_JSON_STREAM_EXCEPTIONS_H
