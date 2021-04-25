d58fc282d01a75b9fe398640fdceb5baed12f971
rez <rezfd99@gmail.com>
2021-04-25 20:53:06 +0430

    reformat array_type.cpp

--- src/types/array_type.cpp
+++ src/types/array_type.cpp
@@ -53,7 +53,7 @@ namespace simple_json::types {
                 default:
                     os << item;
             }
-            os << (item == *std::prev(end()) ? "" : ",");
+            os << (item == * std::prev(end()) ? "" : ",");
         }
         os << '\n' << std::setw(local_indent - indent_length + 1) << ']';
         return os;
