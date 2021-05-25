5ac31c7b00f4a6f0dc07e788efaba3d6c3ebf913
NitroZeus <realantiwanted@gmail.com>
2021-05-25 19:52:06 +0430

    Small refactor

--- src/types/json_object.cpp
+++ src/types/json_object.cpp
@@ -6,7 +6,7 @@
 namespace simple_json::types {
     #pragma region Constructors
 
-    JsonObject::JsonObject(const json_list_type &initializer_list) : map_type(initializer_list) {}
+    JsonObject::JsonObject(const json_list_type & initializer_list) : map_type(initializer_list) {}
 
     #pragma endregion
 
