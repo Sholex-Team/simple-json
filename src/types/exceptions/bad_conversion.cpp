a695c88e25c31959781bbd4e3ce86d99dfcf016f
NitroZeus <realantiwanted@gmail.com>
2021-07-02 20:18:03 +0430

    Small edit

--- src/types/exceptions/bad_conversion.cpp
+++ src/types/exceptions/bad_conversion.cpp
@@ -4,7 +4,7 @@ namespace simple_json::types::exceptions {
     #pragma region Constructors
 
     BadConversion::BadConversion(const DataType src, const DataType dst) noexcept : JsonException(),
-            error_text {new std::string {"Invalid conversion tried from "}} {
+            error_text {new std::string {"Invalid conversion attempt from "}} {
         error_text->append(utils::json_type_to_string(src));
         error_text->append(" type to ");
         error_text->append(utils::json_type_to_string(dst));
